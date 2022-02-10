f5c9af8e830adbcced5b8647760df136b20786c4
meeeerkat <meeeerkat@protonmail.com>
2022-02-10 22:27:38 +0100

    up key touch io works, need to add other keys and speed config

--- main/bt_hid_task.c
+++ main/bt_hid_task.c
@@ -4,10 +4,116 @@
 #include "esp_hidd.h"
 #include "esp_hid_gap.h"
 
+#include "freertos/FreeRTOS.h"
+#include "sdkconfig.h"
+#include "driver/gpio.h"
+#include "driver/touch_pad.h"
+#include "soc/rtc_periph.h"
+#include "soc/sens_periph.h"
+
+#define TOUCH_THRESH_NO_USE   (0)
+#define TOUCHPAD_FILTER_TOUCH_PERIOD (10)
+
+static bool s_pad_activated[TOUCH_PAD_MAX];
+static uint32_t s_pad_init_val[TOUCH_PAD_MAX];
+
+/*
+  Read values sensed at all available touch pads.
+  Use 2 / 3 of read value as the threshold
+  to trigger interrupt when the pad is touched.
+  Note: this routine demonstrates a simple way
+  to configure activation threshold for the touch pads.
+  Do not touch any pads when this routine
+  is running (on application start).
+ */
+static void init_touch_pad_thresholds(void)
+{
+    uint16_t touch_value;
+    for (int i = 0; i < TOUCH_PAD_MAX; i++) {
+        //read filtered value
+        touch_pad_read_filtered(i, &touch_value);
+        s_pad_init_val[i] = touch_value;
+        //set interrupt threshold.
+        ESP_ERROR_CHECK(touch_pad_set_thresh(i, touch_value * 2 / 3));
+    }
+}
+
+
+/*
+  Check if any of touch pads has been activated
+  by reading a table updated by rtc_intr()
+  If so, then print it out on a serial monitor.
+  Clear related entry in the table afterwards
+
+  In interrupt mode, the table is updated in touch ISR.
+static void input_read_task(void *pvParameter)
+{
+    while (1) {
+        if (s_pad_activated[TOUCH_IO] == true) {
+            gpio_set_level(LED_GPIO, 1);
+            s_pad_activated[TOUCH_IO] = false;
+        }
+        else
+            gpio_set_level(LED_GPIO, 0);
+
+        // Wait a while for the pad being released
+        vTaskDelay(pdMS_TO_TICKS(REFRESH_INPUT_DELTA));
+    }
+
+}
+ */
+
+/*
+  Handle an interrupt triggered when a pad is touched.
+  Recognize what pad has been touched and save it in a table.
+ */
+static void rtc_intr(void *arg)
+{
+    uint32_t pad_intr = touch_pad_get_status();
+    //clear interrupt
+    touch_pad_clear_status();
+    for (int i = 0; i < TOUCH_PAD_MAX; i++) {
+        if ((pad_intr >> i) & 0x01) {
+            s_pad_activated[i] = true;
+        }
+    }
+}
+
+void init_inputs(void)
+{
+    // Initialize touch pad peripheral, it will start a timer to run a filter
+    ESP_ERROR_CHECK(touch_pad_init());
+    // If use interrupt trigger mode, should set touch sensor FSM mode at 'TOUCH_FSM_MODE_TIMER'.
+    touch_pad_set_fsm_mode(TOUCH_FSM_MODE_TIMER);
+    // Set reference voltage for charging/discharging
+    // For most usage scenarios, we recommend using the following combination:
+    // the high reference valtage will be 2.7V - 1V = 1.7V, The low reference voltage will be 0.5V.
+    touch_pad_set_voltage(TOUCH_HVOLT_2V7, TOUCH_LVOLT_0V5, TOUCH_HVOLT_ATTEN_1V);
+
+    // Init touch pad IO
+    for (int i = 0; i < TOUCH_PAD_MAX; i++) {
+        //init RTC IO and mode for touch pad.
+        touch_pad_config(i, TOUCH_THRESH_NO_USE);
+    }
+
+    // Initialize and start a software filter to detect slight change of capacitance.
+    touch_pad_filter_start(TOUCHPAD_FILTER_TOUCH_PERIOD);
+    // Init threshholds
+    init_touch_pad_thresholds();
+
+    // Register touch interrupt ISR
+    touch_pad_isr_register(rtc_intr, NULL);
+    
+    // Enabling interupt 
+    touch_pad_intr_enable();
+}
 
 #define RESET_BUFFER for (int i = 0; i < 4; i++) buffer[i] = 0;
 void bt_hid_task(void *pvParameters)
 {
+    // first thing done is initialising touch_pad
+    init_inputs();
+
     esp_hidd_dev_t *hid_dev = pvParameters;
 
     printf("wasdqe, type something\n");
@@ -15,10 +121,13 @@ void bt_hid_task(void *pvParameters)
     static uint8_t buffer[4] = {0};
     RESET_BUFFER
 
-    char c;
-    bool is_input_set = true;
     while (1) {
-        c = fgetc(stdin);
+        if (s_pad_activated[CONFIG_UP_KEY_TOUCH_IO] == true)
+            buffer[2] = -10;
+        s_pad_activated[CONFIG_UP_KEY_TOUCH_IO] = false;
+
+        /*
+        char c = fgetc(stdin);
         switch (c) {
             case 'q':   // left click
                 buffer[0] = 1;
@@ -39,18 +148,16 @@ void bt_hid_task(void *pvParameters)
                 buffer[1] = 10;
                 break;
             default:
-                is_input_set = false;
                 break;
         }
+        */
 
-        if (is_input_set) {
+        if (buffer[0] != 0 || buffer[1] != 0 || buffer[2] != 0 || buffer[3] != 0) {
             esp_hidd_dev_input_set(hid_dev, 0, 0, buffer, 4);
             RESET_BUFFER
         }
-        else
-            is_input_set = true;
 
-        vTaskDelay(pdMS_TO_TICKS(50));
+        vTaskDelay(pdMS_TO_TICKS(CONFIG_REFRESH_INPUT_DELTA));
     }
 }
     
